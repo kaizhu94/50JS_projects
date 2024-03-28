6f9605b2ee013612c73265ac9b76a794a8ada353
kaizhu94 <zhukai0803@gmail.com>
2024-03-27 23:35:33 -0400

    Add simple JS function

--- FQA_Boxes/script.js
+++ FQA_Boxes/script.js
@@ -1,7 +1,7 @@
-const toggleBtns = document.querySelectorAll('.faq-toggle');
+const toggles = document.querySelectorAll('.faq-toggle');
 
-toggleBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        btn.parentNode.classList.toggle('active');
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active');
     })
 })
