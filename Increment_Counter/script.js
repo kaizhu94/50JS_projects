9c023e23b8e06250d07612044c181971ed9b3f82
kaizhu94 <zhukai0803@gmail.com>
2024-04-11 23:37:40 -0400

    rename variables

--- Increment_Counter/script.js
+++ Increment_Counter/script.js
@@ -5,12 +5,12 @@ counters.forEach(counter => {
 
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target')
-        const c = +counter.innerText
+        const currentCounter = +counter.innerText
       
         const increment = target / 200;
   
-        if(c < target) {
-            counter.innerText = `${Math.ceil(c + increment)}`
+        if(currentCounter < target) {
+            counter.innerText = `${Math.ceil(currentCounter + increment)}`
             setTimeout(updateCounter, 1)
         } else {
             counter.innerText = target;
