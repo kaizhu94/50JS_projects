d2083321a44b77586ae97cd9756da2b500e88d48
kaizhu94 <zhukai0803@gmail.com>
2024-05-29 10:56:27 -0400

    Add Canvas

---
+++ Drawing_App/script.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
+const ctx = canvas.getContext('2d');
+
+let size = 10
+let isPressed = false
+colorEl.value = 'black'
+let color = colorEl.value
+let x
+let y
