0d3d0fcd95c99c96bb2a731be80966f3e776ffba
kaizhu94 <zhukai0803@gmail.com>
2024-05-30 23:04:00 -0400

    Mouse Move

--- Drawing_App/script.js
+++ Drawing_App/script.js
@@ -20,3 +20,24 @@ canvas.addEventListener('mousedown', (e) => {
     x = e.offsetX
     y = e.offsetY
 })
+
+
+document.addEventListener('mouseup', (e) => {
+    isPressed = false
+
+    x = undefined
+    y = undefined
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if(isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = x2
+        y = y2
+    }
+})
